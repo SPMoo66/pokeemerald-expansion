8c8dcfee2c06bbb913a3a81d137f4fdd9a293ee7
Moo <steven.peltier12@gmail.com>
2024-07-01 15:55:48 -0400

    Add mughot support and test os image -credit Anon822 for mugshot code

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -58,6 +58,7 @@
 #include "constants/vars.h"
 #include "constants/weather.h"
 #include "constants/pokevial.h" //Pokevial Branch
+#include "constants/mugshots.h"
 	.include "asm/macros.inc"
 	.include "asm/macros/event.inc"
 	.include "constants/constants.inc"

1b946881cb74d8b42750b7dcacd512b228a08490
Moo <steven.peltier12@gmail.com>
2025-03-15 08:51:50 -0400

    Add Rocket Refuge items

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1223,11 +1223,11 @@
 #define FLAG_ITEM_REALMS_FRAY_MEGA_RING                             0x493
 #define FLAG_ITEM_DEVONCORP_NUGGET                                  0x494
 #define FLAG_ITEM_DEVONCORP_BOTTLE_CAP                              0x495
+#define FLAG_ITEM_ROCKET_REFUGE_B1F_NUGGET                          0x496
+#define FLAG_ITEM_ROCKET_REFUGE_B1F_STAR_PIECE                      0x497
+#define FLAG_ITEM_ROCKET_REFUGE_B2F_NUGGET                          0x498
+#define FLAG_ITEM_ROCKET_REFUGE_B2F_PEARL_STRING                    0x499
 
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
-#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
-#define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
 #define FLAG_UNUSED_0x49C                                           0x49C // Unused Flag

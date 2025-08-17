eacb628c4fe46ed4e4cac459f4d5eeed11e46fcf
Moo <steven.peltier12@gmail.com>
2025-08-16 22:26:44 -0400

    Add Fly to field move list as long as badge 2 is obtained

--- src/party_menu.c
+++ src/party_menu.c
@@ -3050,11 +3050,15 @@ static void SetPartyMonFieldSelectionActions(struct Pokemon *mons, u8 slotId)
         {
             if (GetMonData(&mons[slotId], i + MON_DATA_MOVE1) == sFieldMoves[j])
             {
-                AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, j + MENU_FIELD_MOVES);
-                break;
+                if (sFieldMoves[j] != MOVE_FLY) // If Mon already knows FLY, prevent it from being added to action list
+                    AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, j + MENU_FIELD_MOVES);
+                    break;
             }
         }
     }
+
+    if (sPartyMenuInternal->numActions < 5 && FlagGet(FLAG_BADGE02_GET)) // Once Badge 2 is obtained add fly to mon menu
+        AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, 5 + MENU_FIELD_MOVES);
 
     if (!InBattlePike())
     {

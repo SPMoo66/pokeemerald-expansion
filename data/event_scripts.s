9755893bc53f2e332c2096660bc38a3c0127c723
Moo <steven.peltier12@gmail.com>
2026-06-19 22:26:41 -0400

    scene 08

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1738,6 +1738,7 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/expansion_1/scene05.inc"
 	.include "data/expansion_1/follower02.inc"
 	.include "data/expansion_1/scene07.inc"
+	.include "data/expansion_1/scene08.inc"
 
 	.include "data/maps/LeafyClearing/scripts.inc"
 	.include "data/maps/CaveOfDreams/scripts.inc"

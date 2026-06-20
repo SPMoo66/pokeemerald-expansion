2877bc9266f518847e4fcbcd6d39d15e11f1c21b
Moo <steven.peltier12@gmail.com>
2026-06-20 14:35:41 -0400

    Fix regression with FLAG_BLOCK_PLAYER_ACTIONS and add second room signage

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -912,6 +912,7 @@ static void PlayerNotOnBikeMoving(enum Direction direction, u16 heldKeys)
     }
     if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER)
      && (heldKeys & B_BUTTON)
+     && !FlagGet(FLAG_BLOCK_PLAYER_ACTIONS)
      && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0
      && !FollowerNPCComingThroughDoor()
      && (I_ORAS_DOWSING_FLAG == 0 || (I_ORAS_DOWSING_FLAG != 0 && !FlagGet(I_ORAS_DOWSING_FLAG))))
